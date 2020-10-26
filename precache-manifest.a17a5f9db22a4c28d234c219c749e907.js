c8734e0ddec2186cd64a686d4c10ade4750473df
szkapec <szkapec999@o2.pl>
2020-10-26 17:19:39 +0100

    Updates

---
+++ precache-manifest.a17a5f9db22a4c28d234c219c749e907.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e1e5f1dfcfdc28a6eb6fb0d7d21459",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "3b8ffaae1869d84d4316",
+    "url": "/movie-search/static/css/main.7b10e862.chunk.css"
+  },
+  {
+    "revision": "11d05e4b9ba0dbba898b",
+    "url": "/movie-search/static/js/2.d0c38e62.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie-search/static/js/2.d0c38e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8ffaae1869d84d4316",
+    "url": "/movie-search/static/js/main.7d2d7db6.chunk.js"
+  },
+  {
+    "revision": "e0addf519a6b6712f819",
+    "url": "/movie-search/static/js/runtime-main.a5da70ae.js"
+  },
+  {
+    "revision": "1fb831de1a64c283cc2e380acfc07ddf",
+    "url": "/movie-search/static/media/play.1fb831de.png"
+  },
+  {
+    "revision": "45dd54703cf1537d439bc358c680762c",
+    "url": "/movie-search/static/media/star.45dd5470.png"
+  }
+]);
